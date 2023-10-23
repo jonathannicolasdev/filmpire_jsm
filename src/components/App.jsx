c3442433d53f5c3286e4602b12c5fc54b9446186
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-10-23 00:56:47 -0400

    feat: intro and docs

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+// import { Route, Switch } from 'react-router-dom';
+
+const App = () => (
+  <>
+    <CssBaseline />
+    <h1>Hello, World - Filmpire</h1>
+  </>
+);
+
+export default App;
