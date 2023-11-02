962292f928835216450d0d35054d81b26b46a509
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-11-02 10:41:23 -0400

    feat: rendering movies

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as MovieInformation } from './MovieInformation/MovieInformation
 export { default as Movies } from './Movies/Movies';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as Profile } from './Profile/Profile';
+// eslint-disable-next-line import/no-cycle
 export { default as NavBar } from './NavBar/NavBar';
 export { default as MovieList } from './MovieList/MovieList';
 export { default as Movie } from './Movie/Movie';
