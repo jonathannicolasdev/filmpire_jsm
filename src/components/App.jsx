df70418c63c19a28c0879a702c7cac61e7a19a9f
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-11-15 23:40:51 -0500

    feat: into & doc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
 
 export const App = () => (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <h1>Hello world filmpire</h1>
+  </div>
 );
 export default App;
