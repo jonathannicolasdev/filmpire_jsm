6f52b0c3b2fc84cd02c8dc7827190b4d7ac36977
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-11-15 23:17:45 -0500

    feat: dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// eslint-disable-next-line import/no-named-as-default
-import App from './components/App';
+import { App } from './components/App';
 
 // eslint-disable-next-line react/no-deprecated
 ReactDOM.render(<App />, document.getElementById('root'));
