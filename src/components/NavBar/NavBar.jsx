e99150fc0756fe58ec120d217648c8db4ef3dff5
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2024-01-05 21:24:54 -0500

    feat: implement nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery, Icon } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7, ClassRounded } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+
+import useStyles from './styles';
 
 const NavBar = () => {
-  console.log('NavBar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px');
+  const theme = useTheme();
+  const isAuthenticated = true;
   return (
-    <div>NavBar</div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/123"
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV2kMZeVfGNRJdzUaADTJmDj79w6lDzgcXTA&usqp=CAU"
+
+                >avatar
+                </Avatar>
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
